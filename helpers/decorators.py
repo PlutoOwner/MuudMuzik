79fd96f432c78a33bc2460789d3dc92272cc3059
PlutoOwner <123556006+PlutoOwner@users.noreply.github.com>
2023-04-07 20:45:45 +0300

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -4,7 +4,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 
 from helpers.admins import get_administrators
-from config import SUDO_USERS
+from config import OWNER_ID 
 
 
 def errors(func: Callable) -> Callable:
@@ -19,7 +19,7 @@ def errors(func: Callable) -> Callable:
 
 def authorized_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
+        if message.from_user.id in OWNER_ID:
             return await func(client, message)
 
         administrators = await get_administrators(message.chat)
