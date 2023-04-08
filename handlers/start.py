f0783c3fbac051366c20c1f3cffd4e7f415d785d
PlutoOwner <123556006+PlutoOwner@users.noreply.github.com>
2023-04-09 00:00:10 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -15,7 +15,7 @@ from config import (
 @Client.on_message(filters.private & filters.incoming & filters.command(["start", f"start@{BOT_USERNAME}"]))
 async def start(_, message: Message):
       await message.reply_text(f"""**Merhaba {message.from_user.mention} 🎵\nBen {BOT_NAME}!\nSesli sohbetlerde müzik çalabilen botum.\n\nBan yetkisiz, Ses yönetimi yetkisi verip, Asistanı gruba ekleyiniz.**"""),
-         reply_markup=InlineKeyboardMarkup(
+      reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
