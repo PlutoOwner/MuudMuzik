d8069124f81d1c4db0600e7c811aed3b9aab14a3
PlutoOwner <123556006+PlutoOwner@users.noreply.github.com>
2023-04-09 00:01:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-THUMB_IMG = getenv("THUMB_IMG", "https://graph.org/file/adc43233e02aa20cdc6fc.jpg")
-START_IMAGE = getenv("START_IMAGE", "https://graph.org/file/fa5ccd7123f36f9e12592.jpg")
+THUMB_IMG = getenv("THUMB_IMG", "https://graph.org/file/fa5ccd7123f36f9e12592.jpg")
 BOT_NAME = getenv("BOT_NAME", "🎧 Muud Müzik")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
